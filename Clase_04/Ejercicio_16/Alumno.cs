6d953c4ebb0edae3d44e130c3344b26e1e4ead09
alumno <alumno>
2018-08-30 22:10:55 -0300

    Clase 4

---
+++ Clase_04/Ejercicio_16/Alumno.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Ejercicio_16
+{
+    class Alumno
+    {
+        private byte _nota1;
+        private byte _nota2;
+        private float _notaFinal;
+        public string apellido;
+        public int legajo;
+        public string nombre;
+
+        //CONSTRUCTOR
+        //public Alumno(int leg, string apell, string nomb)
+        //{
+        //    this.legajo = leg;
+        //    this.apellido = apell;
+        //    this.nombre = nomb;
+        //}
+
+        public void CalcularFinal()
+        {
+            if (_nota1 >= 4 && _nota2 >= 4)
+            {
+                Thread.Sleep(150);
+                Random notaFinal = new Random();
+                this._notaFinal = notaFinal.Next();
+            }
+            else this._notaFinal = -1;
+        }
+
+        public void Estudiar(byte notaUno, byte notaDos)
+        {
+            this._nota1 = notaUno;
+            this._nota2 = notaDos;
+        }
+
+        public void Mostrar()
+        {
+            if (_notaFinal != -1)
+            {
+                Console.WriteLine("{0}{1}{2}{3}{5}", legajo, nombre, apellido, _nota1, _nota2, _notaFinal);
+            }
+            else Console.WriteLine("{0}{1}{2}{3}{5} Alumno desaprobado.", legajo, nombre, apellido, _nota1, _nota2);
+        }
+    }
+}
---
+++ Clase_04/Ejercicio_16/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio Nro 16";
+
+            byte notaUno;
+            byte notaDos;
+
+            byte nota1;
+            byte nota2;
+
+            //CON CONSTRUCTOR
+            //Alumno alumnoUno = new Alumno(001, "Perez ", "Juan");
+            //Alumno alumnoDos = new Alumno(002, "Gomez ", "Pedro");
+            //Alumno alumnoTres = new Alumno(003, "Lopez ", "Damian");
+
+            Alumno alumnoUno = new Alumno();
+            Alumno alumnoDos = new Alumno();
+            Alumno alumnoTres = new Alumno();
+
+            alumnoUno.nombre = "Juan";
+            alumnoUno.apellido = "Perez ";
+            alumnoUno.legajo = 101;
+
+            alumnoDos.nombre = "Pedro";
+            alumnoDos.apellido = "Gomez ";
+            alumnoDos.legajo = 102;
+
+            alumnoTres.nombre = "Damian";
+            alumnoTres.apellido = "Lopez ";
+            alumnoTres.legajo = 103;
+
+            Console.WriteLine("Ingrese primer nota de {0}{1}", alumnoUno.apellido, alumnoUno.nombre);
+            notaUno = byte.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese segunda nota de {0}{1}", alumnoUno.apellido, alumnoUno.nombre);
+            notaDos = byte.Parse(Console.ReadLine());
+            alumnoUno.Estudiar(notaUno, notaDos);
+
+            Console.WriteLine("Ingrese primer nota de {0}{1}", alumnoDos.apellido, alumnoDos.nombre);
+            nota1 = byte.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese segunda nota de {0}{1}", alumnoDos.apellido, alumnoDos.nombre);
+            nota2 = byte.Parse(Console.ReadLine());
+            alumnoDos.Estudiar(nota1, nota2);
+
+            Console.WriteLine("Ingrese primer nota de {0}{1}", alumnoTres.apellido, alumnoTres.nombre);
+            notaUno = byte.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese segunda nota de {0}{1}", alumnoTres.apellido, alumnoTres.nombre);
+            notaDos = byte.Parse(Console.ReadLine());
+            alumnoTres.Estudiar(notaUno, notaDos);
+
+            Console.WriteLine("Lista de alumnos.");
+            Console.WriteLine("Legajo-----Apellido-----Nombre-----Primera Nota-----Segunda Nota-----Nota Final");
+            alumnoUno.Mostrar();
+            alumnoDos.Mostrar();
+            alumnoTres.Mostrar();
+
+            Console.ReadKey();
+        }
+    }
+}
