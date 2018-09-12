0d0de234cc190567e900d4fecddbbaaaaf6811ad
ezzee01 <ezec_01@hotmail.com>
2018-09-12 01:58:12 -0300

    tp1

---
+++ Entidades/Entidades/Calculadora.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    class Calculadora
+    {
+        private static string ValidarOperador(string operador)
+        {
+            if (string.Equals(operador, "+") == true || string.Equals(operador, "-") == true || string.Equals(operador, "*") == true || string.Equals(operador, "/") == true)
+            {
+                return operador;
+            }
+            else return "+";
+        }
+
+        public double Operar(Numero num1, Numero num2, string operador)
+        {
+            //double resultado;
+
+            //switch(Calculadora.ValidarOperador(operador))
+            //{
+            //    case "+": resultado = num1 + num2;
+            //        break;
+            //    case "-": resultado = num1 - num2;
+            //        break;
+            //    case "*": resultado = num1 * num2;
+            //        break;
+            //    case "/": resultado = num1 / num2;                    
+            //        break;
+            //}
+            //return resultado;
+        }
+    }
+}
---
+++ Entidades/Entidades/Numero.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    class Numero
+    {
+        private double numero;
+
+        private static double ValidarNumero(string strNumero)
+        {
+            double numero;
+
+            if (double.TryParse(strNumero, out numero))
+            {
+                return numero;
+            }
+            else return (double) 0;
+        }
+
+        public double SetNumero()
+        {
+            
+        }
+    }
+}
