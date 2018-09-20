031b85806739523b96fcdcefe80ccbaf9b1cadc4
ezzee01 <ezec_01@hotmail.com>
2018-09-20 02:14:55 -0300

    clase 9

---
+++ Entidades/Entidades/Numero.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+   public class Numero
+    {
+        private double numero;
+
+        private double SetNumero
+        {
+            set
+            {
+                this.numero = value;
+            }
+        }
+
+        public Numero()
+        {
+            this.numero = 0;
+        }
+
+        public Numero(double numero)
+        {
+            this.numero = numero;
+        }
+
+        public Numero(string strNumero) : this(ValidarNumero(strNumero))
+        { }
+
+        private static double ValidarNumero(string strNumero)
+        {
+            double numero = 0;
+            bool resultadoValidacion = false;
+
+            foreach(var num in strNumero)
+            {
+                if (num >= 48 && num <= 57 || double.TryParse(strNumero,out numero))
+                {
+                    resultadoValidacion = true;
+                }
+                else
+                {
+                    resultadoValidacion = false;
+                    break;
+                }
+            }
+            
+            if (resultadoValidacion == true)
+            {
+                numero = double.Parse(strNumero);
+                return numero;
+            }
+            else return numero;
+        }
+
+        public string BinarioDecimal(string binario)
+        {
+            int numeroDecimal = 0;
+            string resultado = "Valor inválido";
+            bool esBinario = false;
+            
+            foreach(var num in binario)
+            {
+                if(num == '1' || num == '0')
+                {
+                    esBinario = true;
+                }
+                else
+                {
+                    esBinario = false;
+                    break;
+                }
+            }
+
+            if (esBinario == true)
+            {
+                resultado = "";
+                for(int i =1; i<= binario.Length; i++)
+                {
+                    if (binario.ElementAt(i - 1) == '1')
+                    {
+                        numeroDecimal += (int) Math.Pow(2, binario.Length - i);
+                    }
+                }
+                resultado = numeroDecimal.ToString();
+                return resultado;
+            }
+            else return resultado;
+        }
+
+        public string DecimalBinario(double numero)
+        {
+            string binario = "";
+            int numeroEntero = (int)numero;
+            
+            if(numeroEntero <= 0)
+            {
+                binario = "Valor inválido";
+            }
+            while(numeroEntero > 0)
+            {
+                binario = (numeroEntero % 2).ToString() + binario;
+                numeroEntero = numeroEntero / 2;
+            }
+
+            return binario;
+        }
+
+        public string DecimalBinario(string numero)
+        {
+            string resultado = "Valor inválido";
+
+            if (numero != "" && numero != "Valor inválido")
+            {
+                resultado = DecimalBinario(double.Parse(numero));
+                return resultado;
+            }
+            else return resultado;            
+        }
+
+        public static double operator +(Numero n1, Numero n2)
+        {
+            return n1.numero + n2.numero;
+        }
+
+        public static double operator -(Numero n1, Numero n2)
+        {
+            return n1.numero - n2.numero;
+        }
+
+        public static double operator *(Numero n1, Numero n2)
+        {
+            return n1.numero * n2.numero;
+        }
+
+        public static double operator /(Numero n1, Numero n2)
+        {
+            if (n2.numero != 0)
+            {
+                return n1.numero / n2.numero;
+            }
+            else return double.MinValue;
+        }
+    }
+}
---
+++ Entidades/MiCalculadora/FormCalculadora.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entidades;
+
+namespace MiCalculadora
+{
+    public partial class FormCalculadora : Form
+    {
+        public FormCalculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        { }
+
+        private void txtNumero1_TextChanged(object sender, EventArgs e)
+        { }
+
+        private void cmbOperador_SelectedIndexChanged(object sender, EventArgs e)
+        { }
+
+        private void txtNumero2_TextChanged(object sender, EventArgs e)
+        { }
+        
+        private void lblResultado_Click(object sender, EventArgs e)
+        { }
+
+        private void btnOperar_Click(object sender, EventArgs e)
+        {
+            double resultado = Operar(txtNumero1.Text, txtNumero2.Text, cmbOperador.Text);
+            lblResultado.Text = resultado.ToString();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnConvertirABinario_Click(object sender, EventArgs e)
+        {
+            string resultado = lblResultado.Text;
+            Numero numero = new Numero(resultado);
+
+            lblResultado.Text = numero.DecimalBinario(resultado);
+        }
+
+        private void btnConvertirADecimal_Click(object sender, EventArgs e)
+        {
+            string resultado = lblResultado.Text;
+            Numero numero = new Numero(resultado);            
+            lblResultado.Text = numero.BinarioDecimal(resultado);
+        }
+
+        private static double Operar(string numero1, string numero2, string operador)
+        {
+            Numero num1 = new Numero(numero1);
+            Numero num2 = new Numero(numero2);
+
+            return Calculadora.Operar(num1, num2, operador);
+        }
+
+        private void Limpiar()
+        {
+            txtNumero1.Clear();
+            txtNumero2.Clear();
+            lblResultado.ResetText();
+            cmbOperador.ResetText();
+        }
+    }
+}
