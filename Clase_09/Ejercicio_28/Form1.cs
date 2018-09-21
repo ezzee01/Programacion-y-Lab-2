4ece187fba2421436711be3ff4424ae580d5d2e9
alumno <alumno>
2018-09-20 22:12:49 -0300

    clase 10 centralita sin terminar

--- Clase_09/Ejercicio_28/Form1.cs
+++ Clase_09/Ejercicio_28/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,61 +10,63 @@ using System.Windows.Forms;
 
 namespace Ejercicio_28
 {
-    public partial class Form1 : Form
+  public partial class Form1 : Form
+  {
+    public Form1()
     {
-        public Form1()
-        {
-            InitializeComponent();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void rtxtPalabras_TextChanged(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void btnCalcular_Click(object sender, EventArgs e)
-        {
-            //char delimitador = ' ';
-            Dictionary<string, int> diccionario = new Dictionary<string, int>();
-            string[] palabras = rtxtPalabras.Text.Split(' ');
-            int maximo = 0;
-            string mensaje = "";
-            int auxiliar;
-            SortedDictionary<string, int> diccionarioOrdenado = new SortedDictionary<string, int>();
-
-            foreach (string palabra in palabras)
-            {
-                if(diccionario.ContainsKey(palabra))
-                {
-                    diccionario[palabra] = diccionario[palabra] + 1;
-                }
-                else
-                {
-                    diccionario.Add(palabra, 1);
-                }
-            }
-
-            foreach(int valor in diccionario.Values)
-            {
-                if(maximo == 0)
-                {
-                    maximo = valor;
-                }
-                else if(valor > maximo)
-                {
-                    maximo = valor;
-                }
-            }
-
-            foreach(string palabra in diccionario.Keys)
-            {
-                
-            }
-        }
+      InitializeComponent();
     }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    private void rtxtPalabras_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btnCalcular_Click(object sender, EventArgs e)
+    {
+      //char delimitador = ' ';
+      Dictionary<string, int> diccionario = new Dictionary<string, int>();
+      string[] palabras = rtxtPalabras.Text.Split(' ');
+      int maximo = 0;
+      string mensaje = "";
+      int auxiliar;
+      SortedDictionary<string, int> diccionarioOrdenado = new SortedDictionary<string, int>();
+
+      foreach (string palabra in palabras)
+      {
+        if (diccionario.ContainsKey(palabra))
+        {
+          diccionario[palabra] = diccionario[palabra] + 1;
+        }
+        else
+        {
+          diccionario.Add(palabra, 1);
+        }
+      }
+
+      foreach (int valor in diccionario.Values)
+      {
+        if (maximo == 0)
+        {
+          maximo = valor;
+        }
+        else if (valor > maximo)
+        {
+          maximo = valor;
+        }
+      }
+
+      foreach (KeyValuePair<string, int> item in diccionario.OrderByDescending(keyValuePair => keyValuePair.Value).Take(3))
+      {
+        mensaje = mensaje + item.Key + " = " + item.Value + "\n";
+      }
+
+      MessageBox.Show(mensaje);
+    }
+  }
 }
