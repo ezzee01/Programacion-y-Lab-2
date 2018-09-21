dd78df9fb8a2572fb7dd25c752eb5d9d1bc90f10
alumno <alumno>
2018-09-20 22:14:11 -0300

    clase 10 centralita sin terminar

---
+++ CLASE_10 !!!!/CentralTelefonica/CentralitaHerencia/Centralita.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralitaHerencia
+{
+    class Centralita
+    {
+        private List<Llamada> listaDeLLamadas;
+        protected string razonSocial;
+
+        public float GananciasPorLocal
+        {
+            get
+            {
+                return CalcularGanancia(Llamada.TipoLlamada.Local);
+            }
+        }
+
+        public float GananciasPorProvincial
+        {
+            get
+            {
+                return CalcularGanancia(Llamada.TipoLlamada.Provincial);
+            }
+        }
+
+        public float GananciasPorTotal
+        {
+            get
+            {
+                return CalcularGanancia(Llamada.TipoLlamada.Todas);
+            }
+        }
+
+        public List<Llamada> Llamadas
+        {
+            get
+            {
+                return listaDeLLamadas;
+            }
+        }
+
+        public Centralita()
+        {
+            this.listaDeLLamadas = new List<Llamada>();
+        }
+
+        public Centralita(string nombreEmpresa):this()
+        {
+            this.razonSocial = nombreEmpresa;
+        }
+
+        private float CalcularGanancia(Llamada.TipoLlamada tipo)
+        {            
+        }
+
+        public string Mostrar()
+        { }
+
+        public void OrdenarLlamadas()
+        { }
+    }
+}
---
+++ CLASE_10 !!!!/CentralTelefonica/CentralitaHerencia/Provincial.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralitaHerencia
+{
+    class Provincial : Llamada
+    {
+        public enum Franja
+        {
+            Franja_1,
+            Franja_2,
+            Franja_3
+        }
+
+        protected Franja franjaHoraria;
+
+        public float CostoLlamada
+        {
+            get
+            {
+                return CalcularCosto();
+            }
+        }
+
+        public Provincial(Franja miFranja, Llamada llamada):this (llamada.NroOrigen,miFranja,llamada.Duracion,llamada.NroDestino)
+        { }
+
+        public Provincial(string origen, Franja miFranja, float duracion, string destino):base (duracion,destino,origen)
+        {
+            this.franjaHoraria = miFranja;
+        }
+
+        private float CalcularCosto()
+        {
+
+        }
+
+        public string Mostrar()
+        { }
+
+
+    }
+}
