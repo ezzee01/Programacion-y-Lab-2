ae85291b0d8031a81fec8d4f70a64e105f0da004
alumno <alumno>
2018-10-18 19:08:25 -0300

    ej 48

---
+++ Ejercicio_48/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ejercicio_48;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Recibo d1 = new Recibo(250);
+            Factura d2 = new Factura(400);
+            Contabilidad<Factura, Recibo> c = new Contabilidad<Factura, Recibo>();
+
+            c += d2;
+            c += d1;
+
+            Console.Read();
+        }
+    }
+}
---
+++ Ejercicio_48/Ejercicio_48/Contabilidad.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_48
+{
+    public class Contabilidad<T,U> where U : new()
+    {
+        private List<T> egresos;
+        private List<U> ingresos;
+
+        public Contabilidad()
+        {
+            egresos = new List<T>();
+            ingresos = new List<U>();
+        }
+
+        public static Contabilidad<T,U> operator +(Contabilidad<T,U> C, T egreso)
+        {
+            C.egresos.Add(egreso);
+            return C;
+        }
+
+        public static Contabilidad<T,U> operator +(Contabilidad<T,U> c, U ingreso)
+        {
+            c.ingresos.Add(ingreso);
+            return c;
+        }
+    }
+}
